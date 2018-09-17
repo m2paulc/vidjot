51dab4ee3f46c23d5508b027cee774ac6ab1c38c
Paul C <hyp3rsp8c3@gmail.com>
2018-09-17 19:45:11 +0000

    fixed db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds123822.mlab.com:23822/vidsjot-prod' }
+    module.exports = { mongoURI: 'mongodb://paulc:&1R3ad*B0ok@ds123822.mlab.com:23822/vidsjot-prod' }
 } else {
     module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' }
 }
